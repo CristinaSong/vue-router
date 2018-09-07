931f14e023fb7cdbc2e6e5b417237dc963b4999a
songchengcheng <songchengcheng1@100tal.com>
2018-09-07 14:59:49 +0800

    url传值

--- demo/src/Router.js
+++ demo/src/Router.js
@@ -2,41 +2,34 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-const Home = { template: '<div>Home内容</div>' }
-const first = { template: '<div>first内容</div>' }
-const second = { template: '<div>second内容</div>' }
-const hehe={ template: '<div>hehe内容</div>'}
 
 
 const router = new VueRouter({
     mode: 'history',
     base: __dirname,
     routes: [
-        { path: '/',components: {
-            default:Home,
-            left:first,
-            right:second
-        }},
-        { path: '/first', components: {
-            default:hehe,
-            left:first,
-            right:second
-        } },
+        { path: '/' },
+        { path: '/params/:aaa/:bbb' },
+        { path: '/parame-regex/:id(\\d+)' },//正则表达式:包含至少一个数字
     ]
 })
 
 new Vue({
     router,
     template: `
-    <div id='r'>
-    <h1>导航</h1>
-    <ol>
-        <li><router-link to="/">/</router-link></li>
-        <li><router-link to="/first">first</router-link></li>
-    </ol>
-    <router-view class="ssdcw"></router-view>
-    <router-view class="ssdcw" name="left" style="float:left;width:50%;background-color:red;height:300px"></router-view>
-    <router-view class="ssdcw" name="right" style="float:left;width:50%;background-color:blue;height:300px"></router-view>
+    <div>
+        <h1>Good Morning</h1>
+        <ul>
+            <li><router-link to="/">/</router-link></li>
+            <li><router-link to="/params/111/222">/params/111/222</router-link></li>
+            <li><router-link to="/parame-regex/222">/parame-regex/222</router-link></li>
+        </ul>
+        <p>Show</p>
+        <pre>
+            <code>
+                {{JSON.stringify($route,null,2)}}
+            </code>
+         </pre>
     </div>
     `
 }).$mount('#app')
