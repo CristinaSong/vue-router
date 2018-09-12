e6c478e5b624437b5e5dcc6bd8f169f643bbbd1d
songchengcheng <songchengcheng1@100tal.com>
2018-09-12 16:33:41 +0800

    路由的过渡动画

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-//import Router from './Router'
-import transition from './transition.vue'
+import Router from './Router'
+// import transition from './transition.vue'
 
-new Vue({
-    el: '#demo',
-    // data: {
-    //     show: true
-    // }
-    render:xx=>xx(transition)
-})
+// new Vue({
+//     el: '#demo',
+//     // data: {
+//     //     show: true
+//     // }
+//     render:xx=>xx(transition)
+// })
 
