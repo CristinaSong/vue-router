ed3bb4956e28e925380ebaac5d2a58b281a5494d
songchengcheng <songchengcheng1@100tal.com>
2018-09-12 18:06:48 +0800

    路由里的钩子

--- demo/src/Router.js
+++ demo/src/Router.js
@@ -11,13 +11,40 @@ const Home = {
         </div>
     `
 }
+const Page404 = {
+    template: `
+        <div>
+            <h2>Page404</h2>
+            <p>Error:404></p>
+        </div>
+    `,
+    //路由里的钩子
+    beforeRouteEnter:(to,from,next)=>{
+        console.log("beforeRouteEnter_to:",to);
+        console.log("beforeRouteEnter_from:",from);
+        next();
+    },
+    beforeRouteLeave:(to,from,next)=>{
+        console.log("beforeRouteLeave_to",to);
+        console.log("beforeRouteLeave_from",from);
+        next();
+    }
+}
 
 const router = new VueRouter({
     mode: 'history',
     base: __dirname,
     routes: [
         { path: '/', component: Home },
-        { path: '/Parent', component: Parent },
+        { path: '/Parent', component: Parent,
+            beforeEnter:(to,from,next)=>{
+                console.log(to);
+                console.log(from);
+                //next（）//可以，next(false);不可以执行下一步
+                next({path:'/qqwfwf'});//改变路由路径
+            }
+        },
+        {path:'*',component:Page404}//path *
     ]
 })
 
@@ -37,6 +64,7 @@ new Vue({
         <ul>
             <li><router-link to='/'>Home</router-link></li>
             <li><router-link to='/Parent'>Parent</router-link></li>
+            <li><router-link to='/Page404'>Page404</router-link></li>
         </ul>
         <transition :name="aaa" mode="out-in">
         <router-view></router-view>
