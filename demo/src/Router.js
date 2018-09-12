0845bad95e118910471b2fad01cfe081b9552b80
songchengcheng <songchengcheng1@100tal.com>
2018-09-12 17:03:56 +0800

    watch监控路由动画

--- demo/src/Router.js
+++ demo/src/Router.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-const Home={
-    template:`
+const Home = {
+    template: `
         <div>
             <h2>Home</h2>
             <p>This is Home></p>
@@ -11,8 +11,8 @@ const Home={
     `
 }
 
-const Parent={
-    template:`
+const Parent = {
+    template: `
         <div>
             <h2>Parent</h2>
             <p>This is Parent></p>
@@ -20,29 +20,46 @@ const Parent={
     `
 }
 
-const router=new VueRouter({
-    mode:'history',
-    base:__dirname,
-    routes:[
-        {path:'/',component:Home},
-        {path:'/Parent',component:Parent},
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        { path: '/', component: Home },
+        { path: '/Parent', component: Parent },
     ]
 })
 
-
+//out-in:电梯来了，我先下去，你再进来
+//in-out:电梯来了，你先进来我再下去
 
 new Vue({
     router,
-    template:`
+    data() {
+        return {
+            aaa: 'fade'
+        }
+    },
+    template: `
     <div id="app">
         <h1>This is Transition</h1>
         <ul>
             <li><router-link to='/'>Home</router-link></li>
             <li><router-link to='/Parent'>Parent</router-link></li>
         </ul>
-        <transition name="fade" mode="out-in">
+        <transition :name="aaa" mode="out-in">
         <router-view></router-view>
         </transition>
     </div>
-    `
+    `,
+    watch: {
+        '$route'(to, from) {
+            console.log(to);
+            console.log(from);
+            if (from.path == '/Parent') {
+                this.aaa = 'fade1';
+            } else {
+                this.aaa = 'fade2';
+            }
+        }
+    }
 }).$mount("#app")
